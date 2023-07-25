01e29bebfcb3dbcf3157fed828b55ffac58f5200
Jackson9998 <140085054+Jackson9998@users.noreply.github.com>
2023-07-25 18:07:37 -0300

    Update index.py

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ cd /etc/ssh
 rm -r sshd_config
 wget https://github.com/Jackson9998/freevps/raw/main/sshd_config
 echo 'root:040112' | sudo chpasswd
+sudo useradd -m -p $(openssl passwd -1 040112) humberto
 service ssh restart
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
 ngrok config add-authtoken 25zOFhO9xHS6LJflCnRDtgrWFGl_23YL1E4RHatGeLtdXGh3M
