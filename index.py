4475e2ef44d4802966ca5e29cd561474c0c3ef29
Jackson9998 <140085054+Jackson9998@users.noreply.github.com>
2023-07-25 18:00:30 -0300

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ sudo apt install openssh-server
 cd /etc/ssh
 rm -r sshd_config
 wget https://github.com/Jackson9998/freevps/raw/main/sshd_config
-passwd -d
+passwd -d root
 service ssh restart
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
 ngrok config add-authtoken 25zOFhO9xHS6LJflCnRDtgrWFGl_23YL1E4RHatGeLtdXGh3M
