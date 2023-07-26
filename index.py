c44d8cb82d123f775df0dee2552acdc243d9f5ce
Jackson9998 <140085054+Jackson9998@users.noreply.github.com>
2023-07-26 10:08:18 -0300

    Update index.py

--- index.py
+++ index.py
@@ -7,4 +7,5 @@ sudo useradd -m -p $(openssl passwd -1 040112) humberto
 service ssh restart
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
 ngrok config add-authtoken 25zOFhO9xHS6LJflCnRDtgrWFGl_23YL1E4RHatGeLtdXGh3M
-ngrok tcp 22
+screen -dmS Ngrok_Tecnic ngrok tcp 22
+./loop.sh
