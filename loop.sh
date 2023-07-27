75a5d51e3a59e22e82ea6d09324d4e8e93131f73
Jackson9998 <140085054+Jackson9998@users.noreply.github.com>
2023-07-27 09:29:57 -0300

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -1,6 +1,7 @@
 echo CONFIGURADO COM SUCESSO!!!
 sleep 3
-echo Verificando o funcionamento...
+echo PARA SABER O SEU IP ABRA: https://dashboard.ngrok.com/cloud-edge/endpoints 
 sleep 3
-echo Nenhum problema encontrado, refazendo o processo...
+echo Acesse do seguinte modo: ssh (Ip-do-ngrok) -p (Porta-do-Ngrok)
+echo Exemplo: ssh 0.tcp.ngrok.io -p 19283
 ./loop.sh
